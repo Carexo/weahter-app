302695970051ca140c5e69f0ae52185385bd2711
Carexo <marcel.mietla3@gmail.com>
2022-01-01 19:49:44 +0100

    feat: add custom 404 page and sharp package

---
+++ pages/404.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Typography from "../components/atoms/typography/Typography";
+
+const Custom404 = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
+
+  return (
+    <h2
+      style={{
+        margin: "0",
+        position: "absolute",
+        top: "0",
+        zIndex: 100,
+        left: "0",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      404 - Page Not Found
+    </h2>
+  );
+};
+
+export default Custom404;
