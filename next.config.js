b884052d990e56f3dbc686030a1283475250fcae
carexo <marcel@marcel.local>
2024-04-26 22:48:22 +0200

    add exporting next app

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    reactStrictMode: true,
-    output: "export",
-    env: {
-        weatherApiUrl: "https://api.weatherapi.com/v1",
-        weatherApiKey: "61a0653c712244238a9160422221706",
-    },
-    images: {
-        domains: ["cdn.weatherapi.com"]
-    },
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/London",
-                permanent: true,
-            },
-        ];
-    },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    weatherApiUrl: "https://api.weatherapi.com/v1",
+    weatherApiKey: "61a0653c712244238a9160422221706",
+  },
+  images: {
+    domains: ["cdn.weatherapi.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/London",
+        permanent: true,
+      },
+    ];
+  },
 };
